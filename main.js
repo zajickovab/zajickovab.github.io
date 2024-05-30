276a44a2ebde03b10652f4b50363db9da89978c3
zajickovab <96124543+zajickovab@users.noreply.github.com>
2024-05-30 22:16:03 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//navbar
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
@@ -16,3 +17,131 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+//menu section
+const foodb = document.querySelector(".foodb");
+const mArrow = document.querySelector('#marrow');
+const foodl = document.querySelector('.foodl');
+const foodItems = document.querySelectorAll('.foodl li');
+
+window.addEventListener("scroll", function() {
+    foodb.classList.toggle("sticky", this.scrollY > 80);
+});
+
+mArrow.addEventListener('click', () => {
+    foodl.classList.toggle('open');
+});
+
+window.addEventListener('scroll', () => {
+    foodl.classList.remove('open');
+});
+
+foodItems.forEach(item => {
+    item.addEventListener('click', () => {
+        foodl.classList.remove('open');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+            const categoryLinks = document.querySelectorAll('.category-link');
+            const categories = document.querySelectorAll('.category');
+
+            // Při načtení stránky zobrazit pouze kategorii 'appetizers'
+            categories.forEach(category => {
+                category.style.display = (category.id === 'appetizers') ? 'block' : 'none';
+            });
+
+            // Funkce pro aktualizaci kategorií
+            function updateCategory(categoryToShow) {
+                categories.forEach(category => {
+                    category.style.display = (category.id === categoryToShow) ? 'block' : 'none';
+                });
+            }
+
+            // Aktualizace kategorií po kliknutí na navigační odkazy
+            categoryLinks.forEach(link => {
+                link.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    const categoryToShow = this.dataset.category
+        updateCategory(categoryToShow);
+    });
+    });
+    
+    // Získání aktuálního data
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; // Měsíce jsou indexované od 0, proto přidáváme 1
+    const year = currentDate.getFullYear();
+    const formattedDate = `${day}.${month}.${year}`;
+    
+    // Zobrazení aktuálního data ve "Daily Menu"
+    const dateElement = document.getElementById('current-date');
+    dateElement.textContent = `Menu for ${formattedDate}`;
+    });
+    
+    
+    document.addEventListener("DOMContentLoaded", function() {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    
+    // Get the element where the current date will be displayed
+    var currentDateElement = document.getElementById("current-date");
+    
+    // Display the current date
+    currentDateElement.textContent = "Today's Date: " + currentDate.toDateString();
+    
+    // Get the element representing the daily menu section
+    var dailyMenuSection = document.getElementById("daily-menu");
+    
+    // Check if the current time is between 10:00 and 14:00
+    if (currentHour >= 11 && currentHour < 14) {
+    // Show the daily menu section
+    dailyMenuSection.style.display = "block";
+    } else {
+    // Hide the daily menu section
+    dailyMenuSection.style.display = "none";
+    }
+    });
+
+
+    
+
+//faq section
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("label");
+    header.addEventListener("click", () =>{
+        item.classList.toggle("open");
+
+        let description = item.querySelector(".description");
+        if(item.classList.contains("open")){
+            description.style.height = `${description.scrollHeight}px`; //scrollHeight property returns the height of an element including padding , but excluding borders, scrollbar or margin
+            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        }else{
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+        removeOpen(index); //calling the funtion and also passing the index number of the clicked header
+    })
+})
+
+function removeOpen(index1){
+    accordionContent.forEach((item2, index2) => {
+        if(index1 != index2){
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".description");
+            des.style.height = "0px";
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    })
+}
+
+//reservation form
+document.getElementById('reservation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulář byl úspěšně odeslán!');
+    this.reset(); // resetuje formulář
+});
---
+++ menu.js
@@ -0,0 +1,128 @@
+const menuData = {
+    "appetizers": [
+        {
+            "name": "appetizer",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto3.jpg"
+        },
+        {
+            "name": "appetizer",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto3.jpg"
+        },
+        {
+            "name": "appetizer",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto3.jpg"
+        }
+    ],
+    "main": [
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto.jpg"
+        },
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto.jpg"
+        },
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto.jpg"
+        }
+    ],
+    "drinks": [
+        {
+            "name": "drink",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto4.jpg"
+        },
+        {
+            "name": "drink",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto4.jpg"
+        },
+        {
+            "name": "drink",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto4.jpg"
+        }
+    ],
+    "daily-menu": [
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto2.jpg"
+        },
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto2.jpg"
+        },
+        {
+            "name": "meal",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            "price": 15.00,
+            "image": "images/menuphoto2.jpg"
+        }
+    ]
+};
+
+// Funkce pro generování obsahu menu
+function generateMenu(category, items) {
+    const categoryElement = document.getElementById(category);
+    const shopContent = categoryElement.querySelector('.menu-content');
+    
+    items.forEach(item => {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        const image = document.createElement('img');
+        image.src = item.image; // URL obrázku z JSON dat
+        row.appendChild(image);
+
+        const name = document.createElement('h3');
+        name.textContent = item.name; // Název položky z JSON dat
+        row.appendChild(name);
+
+        const description = document.createElement('p');
+        description.textContent = item.description; // Popis položky z JSON dat
+        row.appendChild(description);
+
+        const inText = document.createElement('div');
+        inText.classList.add('in-text');
+
+        const price = document.createElement('div');
+        price.classList.add('price');
+
+        const priceTag = document.createElement('h6');
+        priceTag.textContent = '$' + item.price.toFixed(2); // Cena položky z JSON dat
+        price.appendChild(priceTag);
+
+        inText.appendChild(price);
+        row.appendChild(inText);
+
+        shopContent.appendChild(row);
+    });
+}
+
+// Volání funkce pro každou kategorii menu s odpovídajícími daty z JSON souboru
+generateMenu('appetizers', menuData.appetizers);
+generateMenu('main', menuData.main);
+generateMenu('drinks', menuData.drinks);
+generateMenu('daily-menu', menuData['daily-menu']);
+
+
